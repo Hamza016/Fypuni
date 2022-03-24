9a980be97266619e45cc3f5cc86da1376b4f526f
Hamza <m7781072@gmail.com>
2022-03-24 14:29:27 +0500

    create table in index file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,28 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      
+      <table class="table">
+          <thead>
+              <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td scope="row"></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td scope="row"></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+          </tbody>
+      </table>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
